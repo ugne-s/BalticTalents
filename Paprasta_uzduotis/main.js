534a408afd953ef56c7a636c7cc1c491a814f5ac
Ugne Staniute <u.staniute@gmail.com>
2018-05-05 13:04:25 +0300

    fixed duplicate var declaration

--- Paprasta_uzduotis/main.js
+++ Paprasta_uzduotis/main.js
@@ -3,13 +3,13 @@ function prompts() {
         lastName = "",
         age = "";
     for ( ; firstName == ""; ) {
-        var firstName = prompt("Please enter your first name.");
+        firstName = prompt("Please enter your first name.");
     }
     for ( ; lastName == ""; ) {
-        var lastName = prompt("Please enter your last name.");
+        lastName = prompt("Please enter your last name.");
     }
     for ( ; age == ""; ) {
-        var age = prompt("Please enter your age.");
+        age = prompt("Please enter your age.");
     }
     alert("My name is " + firstName + " " + lastName + " and I am " + age + ".");
 }
